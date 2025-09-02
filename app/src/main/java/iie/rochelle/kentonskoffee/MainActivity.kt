f61ae89c56cee753215e4344c6e322f2db38351c
Cait <caitlinjacobs0815@gmail.com>
2025-09-02 11:49:48 +0200

    back up camera stuff

--- app/src/main/java/iie/rochelle/kentonskoffee/MainActivity.kt
+++ app/src/main/java/iie/rochelle/kentonskoffee/MainActivity.kt
@@ -2,23 +2,31 @@ package iie.rochelle.kentonskoffee
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.Gravity
+import android.view.MenuItem
 import android.view.View
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.navigation.NavigationView
 import iie.rochelle.kentonskoffee.databinding.ActivityMainBinding
+import iie.rochelle.kentonskoffee.databinding.ActivityMainWithNavDrawerBinding
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity(), View.OnClickListener,
+    NavigationView.OnNavigationItemSelectedListener {
     var order = Order()
+    private lateinit var binding : ActivityMainWithNavDrawerBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        val binding = ActivityMainWithNavDrawerBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.imgSb1.setOnClickListener(this)
@@ -27,6 +35,17 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         binding.imgSb4.setOnClickListener(this)
         binding.imgSb5.setOnClickListener(this)
         binding.imgSb6.setOnClickListener(this)
+
+        setSupportActionBar(binding.navToolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+
+        var toggleOnOff = ActionBarDrawerToggle(this, binding.drawerLayout, binding.navToolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        binding.drawerLayout.addDrawerListener(toggleOnOff);
+        toggleOnOff.syncState();
+
+        binding.navView.bringToFront();
+        binding.navView.setNavigationItemSelectedListener(this)
     }
 
     override fun onClick(v: View?) {
@@ -41,4 +60,24 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         Toast.makeText(this@MainActivity, order.productName, Toast.LENGTH_SHORT).show()
         openIntent(applicationContext, order.productName, OrderDetailsActivity::class.java)
     }
+
+        override fun onBackPressed() {
+
+            if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)){
+                binding.drawerLayout.closeDrawer(GravityCompat.START)
+            }
+            else{
+            super.onBackPressed()}
+
+        }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.nav_photo -> openIntent(this, "", CoffeeSnapsActivity::class.java)
+        }
+        binding.drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+
+    }
+
 }
